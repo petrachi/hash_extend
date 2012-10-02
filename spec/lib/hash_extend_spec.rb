52b8ab14cf0fd0a20975dc700e31fd5cafc4fd47
PatrickMa <patrick@multichannel.net>
2012-10-02 09:50:12 +0800

    wrote spec for Hash#delete_many

--- spec/lib/hash_extend_spec.rb
+++ spec/lib/hash_extend_spec.rb
@@ -2,6 +2,7 @@ require './lib/hash_extend'
 
 describe Hash do
   subject { { a: {b: 1}, b: 2 } }
+  let!(:old) { subject }
   describe '#stealth_delete!' do
     it 'returns the pre-delete hash' do
       subject.stealth_delete!(:b).should == subject
@@ -21,6 +22,38 @@ describe Hash do
 
     it 'returns modified hash' do
       subject.map_values! { 2 }.should == { a: 2, b: 2 }
+    end
+  end
+
+  describe '#map_keys' do
+    it 'modifies keys from hash through block' do
+      subject.map_keys { 2 }.should == { 2 => 2 }
+    end
+
+    it 'does not modify the hash' do
+      subject.map_keys { 2 }
+      subject.should == old
+    end
+  end
+  
+  describe '#map_keys!' do
+    it 'modifies the hash changing keys through block' do
+      subject.map_keys! { 2 }
+      subject.should == { 2 => 2 }
+    end
+  end
+  
+  describe '#delete_many' do
+    it 'can delete one key' do
+      subject.delete_many(:a)
+      subject.should_not have_key(:a)
+      subject.should have_key(:b)
+    end
+
+    it 'can delete many keys' do
+      subject.delete_many(:a, :b)
+      subject.should_not have_key(:a)
+      subject.should_not have_key(:b)
     end
   end
 end
